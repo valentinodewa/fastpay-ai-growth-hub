cdd3c4db987984303b2b39b86e1f13ed3d2194f4
valentinodewa <inovalentino99@gmail.com>
2026-07-03 14:28:13 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 window.FASTPAY_CONFIG = Object.freeze({
-  API_URL: 'https://script.google.com/macros/s/AKfycbzYnKh32VybKNLom4npJMVVXOtIZVjXLNcVUKcrulOCVlEPiE5r99-mhd719BiSuVGw/exec' // Isi dengan URL Web App Apps Script yang berakhiran /exec sebelum upload ke GitHub.
+  API_URL: 'https://script.google.com/macros/s/AKfycbz3cd9HlN28f3XZ2yxdJ8FHpjo9B0F1tuF2Z2RDszIp7Xi-7orWEkjav1-SwzDT1t_n/exec' // Isi dengan URL Web App Apps Script yang berakhiran /exec sebelum upload ke GitHub.
 });
 // Trigger GitHub Pages deployment
